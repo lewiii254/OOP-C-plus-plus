34e2b549c667254861b6b0361b9517286f15d661
MARKLEWIS MUTUGI <164312183+lewiii254@users.noreply.github.com>
2025-03-19 00:26:42 +0400

    C++ Real-Life Examples With W3schools😁🚀🔥🔥

---
+++ helloWorld.cpp
@@ -0,0 +1,14 @@
+// Your First C++ Program
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World!";
+    cout << " This is my first C++ program!" << endl;
+    cout << " I'm excited to learn and practice!" << endl;
+    cout << " Let's keep coding and improving!" << endl;
+    cout << " Keep up the great work!" << endl;
+    cout << " Remember to have fun while coding!" << endl;
+    return 0;
+}
