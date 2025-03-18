6da2c65ba75f37e476fd4560cb8346e5b42aad25
MARKLEWIS MUTUGI <164312183+lewiii254@users.noreply.github.com>
2025-03-19 00:37:26 +0400

    Add initial C++ examples for strings, boolean logic, and conditionals🚀😁😁🔥

---
+++ Strings1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string message = "Hey! Good to see you, ";
+  string fname = "Mark lewis";
+
+  cout << message + fname << endl;
+
+  cout << "Welcome to the program!" << endl;
+  
+
+  cout << "Goodbye! Have a great day!" << endl;
+  return 0;
+}
